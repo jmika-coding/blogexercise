4dc4279912022fd8a8541cf34bb4d7d37c29a39a
jmika-coding <mika78500@hotmail.fr>
2020-07-22 12:47:00 +0200

    Add io-ts for Post method, and differentiate post and put with one required arguments, and the other optional + Add another runtime check with io-ts for Put method

--- src/routes.ts
+++ src/routes.ts
@@ -6,16 +6,26 @@ import { fold } from 'fp-ts/lib/Either'
 import { pipe } from 'fp-ts/lib/pipeable'
 import { isRight } from 'fp-ts/lib/Either'
 
-
 // Interfaces for request of post and put and delete routes
 
 const Params = t.type({ id: t.number })
 
 interface RequestBodyDefault {
-  [key: string]: string | number
+  [key: string]: t.TypeOf<typeof t.string> | t.TypeOf<typeof t.number>
 }
 
 const isRequestBody = (s: unknown): s is string => s === "post" || s === "likes" || s === "comment";
+
+interface RequestBodyPost { post: t.TypeOf<typeof t.string>; likes: t.TypeOf<typeof t.number>; comment: t.TypeOf<typeof t.string> }
+
+// With t.type all fields are required, we use it for POST
+const RequestBodyValuesTypePost = t.type({post: t.string, likes: t.number, comment: t.string})
+type RequestBodyValuesTypePost = t.TypeOf<typeof RequestBodyValuesTypePost>
+
+// With t.partial all fields are optional, we use it for put
+// We can combine required and optioanl with t.intersection
+const RequestBodyValuesTypePut = t.partial({post: t.string, likes: t.number, comment: t.string})
+type RequestBodyValuesTypePut = t.TypeOf<typeof RequestBodyValuesTypePut>
 
 // Type<A, O, I>
 const requestBody = new t.Type<string, string, unknown>(
@@ -28,6 +38,11 @@ const requestBody = new t.Type<string, string, unknown>(
 interface RequestGenericInterface {
   Params: t.TypeOf<typeof Params>;
   Body: RequestBodyDefault;
+}
+
+interface RequestGenericInterfaceForPost {
+  Params: t.TypeOf<typeof Params>;
+  Body: RequestBodyPost;
 }
 
 interface RouteGenericInterface extends RequestGenericInterface{};
@@ -47,12 +62,14 @@ export default async function (server:FastifyInstance<Server, IncomingMessage, S
     }
   })
 
-  server.route({method: 'POST', url: '/posts', handler: async (request:FastifyRequest<RouteGenericInterface, Server, IncomingMessage>) => {
+  server.route({method: 'POST', url: '/posts', handler: async (request:FastifyRequest<RequestGenericInterfaceForPost, Server, IncomingMessage>) => {
       try {
-        await knex('blog').insert({post: request.body.post, likes: request.body.likes, comment: request.body.comment})
+        if(isRight(RequestBodyValuesTypePost.decode(request.body))) {
+          await knex('blog').insert({ post: request.body.post, likes: request.body.likes, comment: request.body.comment })
+        } else {throw new Error("Invalid type in body")}
         return "Post created\n"
       } catch(error) {
-        if(error) { return console.error(error); }
+        if(error) { return error; }
       }
     }
   })
@@ -66,7 +83,7 @@ export default async function (server:FastifyInstance<Server, IncomingMessage, S
 
           await Promise.all(Object.keys(request.body).map(async key => {
             const keyDecoded = pipe(requestBody.decode(key), fold(onLeft,  onRight))
-            if(isRight(requestBody.decode(key))){
+            if(isRight(requestBody.decode(key)) && isRight(RequestBodyValuesTypePut.decode(request.body))){
               await knex('blog').where('id', request.params.id).update({[keyDecoded]: request.body[keyDecoded]})
             } else { throw new Error("Invalid type in body") }
         }))
