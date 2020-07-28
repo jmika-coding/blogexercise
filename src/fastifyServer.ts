6529f898eaf172c26e6dbef24aba61955ced0bc4
jmika-coding <mika78500@hotmail.fr>
2020-07-28 14:19:43 +0200

    Add pretty print for pino, by default output to console + add comment in fastifyServer

--- src/fastifyServer.ts
+++ src/fastifyServer.ts
@@ -1,15 +1,6 @@
 import * as fastify from 'fastify'
 import * as pino from 'pino'
-interface FastifyLoggerInstance extends fastify.FastifyLoggerInstance { file: string; timestamp: pino.TimeFn; }
-const server = fastify.fastify<Server, IncomingMessage, ServerResponse, FastifyLoggerInstance>({
-  logger: {
-    level: 'info',
-    file: 'logs.txt', // Will use pino.destination(), name of log will be logs.txt
-    timestamp: pino.stdTimeFunctions.isoTime // ISO 8601-formatted time in UTC
-    // Caution: attempting to format time in-process will significantly impact logging performance.
-    // I don't know if this is the case when specify date format
-  }
-});
+interface FastifyLoggerInstance extends fastify.FastifyLoggerInstance { file: string; timestamp: pino.TimeFn; prettyPrint: boolean; }
 
 import * as Knex from 'knex'
 
@@ -19,14 +10,31 @@ import {PostRoutes} from './controllers/PostRoutesController'
 import {PostRepository} from './persistances/PostRepository'
 import { Server, IncomingMessage, ServerResponse } from 'http';
 
+// CONFIG LOG FASTIFY
+let fileLog = '';
+if(process.argv.length === 3 && process.argv[2] === "logs") { fileLog = "logs.txt"; }
+const server = fastify.fastify<Server, IncomingMessage, ServerResponse, FastifyLoggerInstance>({
+  logger: {
+    level: 'info',
+    file: fileLog, // Will use pino.destination(), name of log will be logs.txt
+    timestamp: pino.stdTimeFunctions.isoTime, // ISO 8601-formatted time in UTC
+    // Caution: attempting to format time in-process will significantly impact logging performance.
+    // I don't know if this is the case when specify date format
+    prettyPrint: true
+  }
+});
+
+// MAIN
 async function main(): Promise<{}>{
 
+  // CONFIG KNEX AND PORT SERVER
   const config = await loadConfigAsync('.env')
   const knex = Knex({
     client: String(config.client.client),
     connection: config.clientParameters
   })
 
+  // START SERVER
   server.log.info("Starting server")
   const postRepository = new PostRepository(knex);
 
@@ -43,10 +51,11 @@ async function main(): Promise<{}>{
     cb(new Error("Not allowed"), false)
   }
   })
-
+  // HERE WE START THE SERVER, WHEN WE CALL MAIN
   return await server.listen(Number(config.http.port))
 }
 
+// CALL MAIN TO START SERVER
 main().catch((err) => {
   server.log.error("Fatal error in fastify server")
   server.log.error(err)
